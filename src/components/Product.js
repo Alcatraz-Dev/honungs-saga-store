f3af64cd8eadc2313e8ce767f001d2a616c67fc4
Haythem Dhahri <alcatraz3019@gmail.com>
2024-11-10 00:54:23 +0100

    change product ui

--- src/components/Product.js
+++ src/components/Product.js
@@ -6,54 +6,57 @@ import comingSoon from '../img/comingSoon.jpg';
 const Product = ({ product }) => {
   console.log(product?.volume)
 
-  return <Link to={`/product/${product?.id}`} className='w-full'>
-    <div className=' grad group w-full h-[362px] rounded-[8px] overflow-hidden relative  '>
-      {/* badge */}
-      {product.isNew ? (
-        <div className='absolute  bg-accent text-primary text-[12px] font-extrabold uppercase top-4 right-4 px-2 rounded-full z-10'>
-          new
-        </div>
-      ) : (
-        ""
-      )}
-      {/* image */}
-      <div className='w-full h-[200px] flex items-center justify-center relative'>
-        {product?.image?.url ? (
-          <img
-            className='w-[150px] h-[150px] group-hover:scale-90 transition-all duration-300 rounded-md'
-            src={`${product?.image?.url}`}
-            alt='product image' />
+  return (
+    <Link to={`/product/${product?.id}`} className='w-full mt-[30px]'>
+      <div className=' grad group w-full h-[362px] rounded-[8px] overflow-hidden relative  '>
+        {/* badge */}
+        {product.isNew ? (
+          <div className='absolute  bg-accent text-primary text-[12px] font-extrabold uppercase top-4 right-4 px-2 rounded-full z-10'>
+            new
+          </div>
         ) : (
-          <img
-            className='w-[150px] h-[150px] group-hover:scale-90 transition-all duration-300 rounded-md'
-            src={comingSoon}
-            alt='product image' />
+          ""
         )}
+        {/* image */}
+        <div className='w-full h-[200px] flex items-center justify-center relative'>
+          {product?.image?.url ? (
+            <img
+              className='w-[150px] h-[150px] group-hover:scale-90 transition-all duration-300 rounded-md'
+              src={`${product?.image?.url}`}
+              alt='product image' />
+          ) : (
+            <img
+              className='w-[150px] h-[150px] group-hover:scale-90 transition-all duration-300 rounded-md'
+              src={comingSoon}
+              alt='product image' />
+          )}
 
-      </div>
+        </div>
 
-      {/* text */}
-      <div className='px-6 pb-8 flex flex-col '>
-        {/* category title */}
-        <div className='text-sm text-accent capitalize mb-2'>
-          {product?.categories?.[0]?.title}
-        </div>
-        {/*  title */}
-        <div className='text-[15px] mb-4 lg:mb-9 line-clamp-1'>
-          {product?.title.substring(0, 35) + "..."}
-        </div>
-        {/*  price & volume */}
-        <div className='flex justify-between items-center'>
-          <div className='text-sm text-white'>
-            {product?.volume}
+        {/* text */}
+        <div className='px-6 pb-8 flex flex-col '>
+          {/* category title */}
+          <div className='text-sm text-accent capitalize mb-2'>
+            {product?.categories?.[0]?.title}
           </div>
-          <div className='text-base text-accent'>
-            {product?.price} kr
+          {/*  title */}
+          <div className='text-[15px] mb-4 lg:mb-9 line-clamp-1'>
+            {product?.title.substring(0, 35) + "..."}
+          </div>
+          {/*  price & volume */}
+          <div className='flex justify-between items-center'>
+            <div className='text-sm text-white'>
+              {product?.volume}
+            </div>
+            <div className='text-base text-accent'>
+              {product?.price} kr
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </Link>;
+    </Link>
+  )
+    ;
 };
 
 export default Product;
