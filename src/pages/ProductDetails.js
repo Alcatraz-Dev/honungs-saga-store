4cffaf7f28b915996e2607fc9e4b1aa0c6a803ef
Haythem Dhahri <alcatraz3019@gmail.com>
2024-11-10 00:59:56 +0100

    fix product details page

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -23,7 +23,7 @@ const ProductDetails = () => {
   const categoryTitle = data[0]?.categories[0]?.title;
   return <div className='mb-16 pt-44 lg:pt-[30px] xl:pt-0'>
     <div className='container mx-auto'>
-      <div className='flex flex-col lg:flex-row gap-[30px] mb-[30px]'>
+      <div className='flex flex-col lg:flex-row gap-[30px] mb-[30px] mt-[30px] '>
         <div className='flex-1 lg:max-w-[40%] lg:h-[540px] grad rounded-lg flex justify-center items-center'>
           {data[0]?.image?.url ? (
             <img className='w-full max-w-[65%] rounded-md'
