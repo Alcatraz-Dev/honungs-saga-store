a386410aa3cff9dee5fc7a77b23a6c67deffdc69
Haythem Dhahri <alcatraz3019@gmail.com>
2024-11-14 14:21:50 +0100

    add default about us text

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -21,7 +21,23 @@ function AboutUs() {
         </p>
       ) : (
         <p className="text-base sm:text-lg lg:text-xl text-gray-200 leading-loose max-w-3xl mx-auto">
-          We are passionate about providing you with the purest, organic wild honey, ethically harvested from untouched, pristine environments. Our commitment to quality means that every jar of honey is carefully sourced and crafted to retain its natural richness, flavor, and nutritional properties.
+          We are passionate about providing you with the purest, organic wild honey, ethically harvested from untouched, pristine environments.
+          Our commitment to quality means that every jar of honey is carefully sourced and crafted to retain its natural richness, flavor, and nutritional properties.
+
+          We are unique on selling honey with fresh blended royal jelly and bee pollen from the same beekeeper ,with a guarantee to result in immunity against flu for our customers.
+          Our quality is one of the worlds highest quality. Improved by our own studies before selling these products online.
+          We are dietist and experts in nutrition.
+          We got different premium honey mixes for immunity, energy, concentration .
+
+          Long time ago the people in the area where this honey comes from used only honey against different diseases.
+
+          The science has improved that these honeys we got are truly good against them.
+
+          The origin of our honey is from the east atlas mountains near the meditereanan sea, Tunisia.
+
+          We can deliver directly our products from the farm in Tunisia or from Europe, Sweden.
+
+          Remember, good honey becomes never old 😉
         </p>
       )}
 
