411d1b9a3a6c47c11a71dec44a0e6614e07a67aa
Haythem Dhahri <alcatraz3019@gmail.com>
2024-11-18 02:18:16 +0100

    check the endponit for fetch data

--- src/components/LanguageSelector.js
+++ src/components/LanguageSelector.js
@@ -8,7 +8,7 @@ const LanguageSelector = () => {
   const [selectedLocale, setSelectedLocale] = useState('sv'); // Default language: Swedish
 
   // Debug fetched data
-  console.log('Data from useFetch:', data.map((locale) => locale.name));
+  console.log('Data from useFetch:', data[0]?.code);
 
   // Handle loading and error states
   if (isLoading) return <p>Loading languages...</p>;
